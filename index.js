d5429cc932f073284ec4d2c6c1ed6e19f95812bf
CH4R4F <developermarghin@gmail.com>
2022-08-15 10:00:41 +0100

    add 3.7: Phonebook backend step7

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const morgan = require("morgan");
 const generateId = require("./helpers").generateId;
 const app = express();
 app.use(express.json());
+app.use(morgan("tiny"));
 
 app.listen(3001, () => console.log("Server is running on port 3001"));
 
