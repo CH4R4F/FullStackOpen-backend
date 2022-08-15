c5b8ef4fc69759d3dccf57ae6148008c640e72ab
CH4R4F <developermarghin@gmail.com>
2022-08-15 07:39:18 +0100

    add 3.2: Phonebook backend step2

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
 const express = require("express");
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-
-app.listen(3001, () => console.log('Server is running on port 3001'))
+app.listen(3001, () => console.log("Server is running on port 3001"));
 
 const data = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/api/persons', (req, res) => {
-	res.status(200).json(data)
-})
+app.get("/api/persons", (req, res) => {
+  res.status(200).json(data);
+});
+
+app.get("/info/", (req, res) => {
+  res.send(`
+    <p>Phonebook has info for ${data.length} people</p>
+    <p>${new Date()}</p> 
+  `);
+});
